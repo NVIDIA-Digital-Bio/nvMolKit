45a7c48da9c30b368490c4fc3d612ce4d98f96c7
Kevin Boyd <kboyd@nvidia.com>
2025-11-10 10:30:01 -0500

    Testing setup

---
+++ admin/setup_conda_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PYTHON_VERSION=$1
+RDKIT_VERSION=$2
+
+
+set -ex
+
+mkdir -p /var/tmp && wget -q -nc --no-check-certificate -P /var/tmp https://github.com/conda-forge/miniforge/releases/download/25.3.0-3/Miniforge3-25.3.0-3-Linux-x86_64.sh && \
+    bash /var/tmp/Miniforge3-25.3.0-3-Linux-x86_64.sh -b -p /usr/local/anaconda && \
+    /usr/local/anaconda/bin/conda init && \
+    ln -s /usr/local/anaconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    . /usr/local/anaconda/etc/profile.d/conda.sh && \
+    conda activate base && \
+    conda config --add channels conda-forge --add channels nvidia --add channels pytorch && \
+    conda install -y  python=$PYTHON_VERSION rdkit=$RDKIT_VERSION libboost-devel libboost-headers libboost-python-devel librdkit-dev pytest pandas psutil scikit-build=0.18
