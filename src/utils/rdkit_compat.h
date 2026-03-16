d1dbd8e57aed875ceb2f92bb9946aaf60729912a
Eva Xue <evax@nvidia.com>
2026-03-16 11:42:46 -0700

    Refactor rdkit_compat.h to use C++20 concepts instead of SFINAE
    Replace SFINAE-based template overloads with a HasValenceType concept
    and if constexpr branching for cleaner compile-time RDKit version
    detection.

--- src/utils/rdkit_compat.h
+++ src/utils/rdkit_compat.h
@@ -18,38 +18,36 @@
 
 #include <GraphMol/Atom.h>
 
-#include <type_traits>
+#include <concepts>
 
 namespace nvMolKit {
 namespace compat {
 
 // RDKit 2025.09+ deprecates getExplicitValence()/getImplicitValence()
-// in favor of getValence(ValenceType). Detect at compile time via SFINAE.
-template <typename T, typename = void> struct HasValenceType : std::false_type {};
-template <typename T> struct HasValenceType<T, std::void_t<typename T::ValenceType>> : std::true_type {};
+// in favor of getValence(ValenceType). Detect at compile time via concept.
+template <typename T>
+concept HasValenceType = requires { typename T::ValenceType; };
 
-template <typename T = RDKit::Atom, std::enable_if_t<HasValenceType<T>::value, int> = 0>
 inline int getExplicitValence(const RDKit::Atom* atom) {
-  return atom->getValence(RDKit::Atom::ValenceType::EXPLICIT);
-}
-template <typename T = RDKit::Atom, std::enable_if_t<!HasValenceType<T>::value, int> = 0>
-inline int             getExplicitValence(const RDKit::Atom* atom) {
+  if constexpr (HasValenceType<RDKit::Atom>) {
+    return atom->getValence(RDKit::Atom::ValenceType::EXPLICIT);
+  } else {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-  return atom->getExplicitValence();
+    return atom->getExplicitValence();
 #pragma GCC diagnostic pop
+  }
 }
 
-template <typename T = RDKit::Atom, std::enable_if_t<HasValenceType<T>::value, int> = 0>
 inline int getImplicitValence(const RDKit::Atom* atom) {
-  return atom->getValence(RDKit::Atom::ValenceType::IMPLICIT);
-}
-template <typename T = RDKit::Atom, std::enable_if_t<!HasValenceType<T>::value, int> = 0>
-inline int             getImplicitValence(const RDKit::Atom* atom) {
+  if constexpr (HasValenceType<RDKit::Atom>) {
+    return atom->getValence(RDKit::Atom::ValenceType::IMPLICIT);
+  } else {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-  return atom->getImplicitValence();
+    return atom->getImplicitValence();
 #pragma GCC diagnostic pop
+  }
 }
 
 }  // namespace compat
